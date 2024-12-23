4088ea22c78f667c4059edea3ab31756e8f65e35
Vishal Madupu <vishalreddy4500>
2024-12-23 18:08:07 +0530

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+
+
+
+'use client';
 import React from "react";
+import dynamic from 'next/dynamic';
 import styles from "./page.module.css";
 
+const TextEditor = dynamic(() => import('@/Component/Editor/Editor'), {
+  ssr: false, // Disable server-side rendering
+});
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello</h1>
+      <TextEditor />
     </div>
   );
 }
